184822170da820a9289aa3a0f8c611c770f97c82
ahfakt <a.farukakts@outlook.com>
2024-09-08 02:57:59 +0300

    .

--- test/Base/src/Base_00.cpp
+++ test/Base/src/Base_00.cpp
@@ -38,8 +38,8 @@ testDecode(char const* fileName, Format::BaseMode mode, int length, int maxChunk
 void
 test(char const* fileName, Format::BaseMode mode, int length, int maxChunkLength)
 {
-	auto encode = testEncode(fileName, mode,  length, maxChunkLength);
-	auto decode = testDecode(fileName, mode,  length, maxChunkLength);
+	auto encode = testEncode(fileName, mode, length, maxChunkLength);
+	auto decode = testDecode(fileName, mode, length, maxChunkLength);
 	assert(encode == decode);
 }
 
@@ -50,21 +50,21 @@ int main() {
 	int length = 1024*64;
 	int maxChunkLength = 256;
 
-	test("base16.enc", Format::BaseMode::BASE16,  length, maxChunkLength);
+	test("base16.enc", Format::BaseMode::BASE16, length, maxChunkLength);
 
 	length += std::uniform_int_distribution<int>{1, 5}(gen);
-	test("base32.enc", Format::BaseMode::BASE32,  length, maxChunkLength);
-	test("base32_npad.enc", Format::BaseMode::BASE32_NPAD,  length, maxChunkLength);
-	test("base32_hex.enc", Format::BaseMode::BASE32_HEX,  length, maxChunkLength);
-	test("base32_hex_npad.enc", Format::BaseMode::BASE32_HEX_NPAD,  length, maxChunkLength);
+	test("base32.enc", Format::BaseMode::BASE32, length, maxChunkLength);
+	test("base32_npad.enc", Format::BaseMode::BASE32_NPAD, length, maxChunkLength);
+	test("base32_hex.enc", Format::BaseMode::BASE32_HEX, length, maxChunkLength);
+	test("base32_hex_npad.enc", Format::BaseMode::BASE32_HEX_NPAD, length, maxChunkLength);
 
 	length += std::uniform_int_distribution<int>{1, 3}(gen);
-	test("base64.enc", Format::BaseMode::BASE64,  length, maxChunkLength);
-	test("base64_npad.enc", Format::BaseMode::BASE64_NPAD,  length, maxChunkLength);
-	test("base64_url.enc", Format::BaseMode::BASE64_URL,  length, maxChunkLength);
-	test("base64_url_npad.enc", Format::BaseMode::BASE64_URL_NPAD,  length, maxChunkLength);
-	test("base64_utf7.enc", Format::BaseMode::BASE64_UTF7,  length, maxChunkLength);
-	test("base64_imap.enc", Format::BaseMode::BASE64_IMAP,  length, maxChunkLength);
+	test("base64.enc", Format::BaseMode::BASE64, length, maxChunkLength);
+	test("base64_npad.enc", Format::BaseMode::BASE64_NPAD, length, maxChunkLength);
+	test("base64_url.enc", Format::BaseMode::BASE64_URL, length, maxChunkLength);
+	test("base64_url_npad.enc", Format::BaseMode::BASE64_URL_NPAD, length, maxChunkLength);
+	test("base64_utf7.enc", Format::BaseMode::BASE64_UTF7, length, maxChunkLength);
+	test("base64_imap.enc", Format::BaseMode::BASE64_IMAP, length, maxChunkLength);
 
 	return 0;
 }
